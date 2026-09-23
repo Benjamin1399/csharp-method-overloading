8bde9bc7c8375f5b9d262e086b5faf25271c1865
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-09-23 22:44:10 +0200

    Created test code

--- EmployeeOverloading/Program.cs
+++ EmployeeOverloading/Program.cs
@@ -11,6 +11,13 @@ namespace EmployeeOverloading
     {
         static void Main(string[] args)
         {
+            EmployeeModel junior = new EmployeeModel("Benjamin", "Naidoo", 72, "IT", 40000);
+
+            EmployeeModel contractor = new EmployeeModel("Tim", "Corey");
+            Console.WriteLine($"{contractor.FirstName} has id {contractor.EmployeeId}");
+
+            EmployeeModel intermediate = new EmployeeModel(junior);
+            Console.WriteLine($"Intermediate copied junior: {intermediate.EmployeeId}, {intermediate.MonthlySalary}");
         }
     }
 }
